f3f8df418dfd19ac08fa97d6be3ff21a08aa752e
matias-humacata <hhumacata@gmail.com>
2024-01-16 04:32:33 -0300

    first commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,22 @@
+
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { Auth0Provider } from "@auth0/auth0-react";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+     <Auth0Provider
+      domain="dev-fegbu63ipevdntas.us.auth0.com"
+      clientId="FibGSihk7DpEm2Dq3G2y8Z621ZUhz2LA"
+      redirectUri={window.location.origin}
+    >
+      <App />
+    </Auth0Provider>
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
